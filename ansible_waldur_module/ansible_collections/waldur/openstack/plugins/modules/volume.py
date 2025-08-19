a2742a2a35d76509e28f6b42a3fba14f24754908
Sergei Zaiaev <sergei.zaiaev@ut.ee>
2025-08-19 02:08:10 +0000

    Update Ansible Waldur modules

--- ansible_waldur_module/ansible_collections/waldur/openstack/plugins/modules/volume.py
+++ ansible_waldur_module/ansible_collections/waldur/openstack/plugins/modules/volume.py
@@ -433,12 +433,12 @@ RUNNER_CONTEXT = {
     "order_poll_url": "/api/marketplace-orders/",
     "terminate_url": "/api/marketplace-resources/",
     "attribute_param_names": [
-        "description",
         "image",
         "type",
+        "description",
+        "name",
         "availability_zone",
         "size",
-        "name",
     ],
     "resolvers": {
         "offering": {
