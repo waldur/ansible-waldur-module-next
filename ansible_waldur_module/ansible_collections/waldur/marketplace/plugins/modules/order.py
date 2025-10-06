58eba0634c8b73302633e8daafec424a17c2096c
Ilja Livenson <ilja@opennodecloud.com>
2025-10-06 14:23:56 +0000

    Update Ansible Waldur modules from develop branch

--- ansible_waldur_module/ansible_collections/waldur/marketplace/plugins/modules/order.py
+++ ansible_waldur_module/ansible_collections/waldur/marketplace/plugins/modules/order.py
@@ -197,7 +197,7 @@ resource:
       description: Provider slug
       type: str
       returned: always
-      sample: a1b2c3d4-e5f6-7890-abcd-ef1234567890
+      sample: string-value
     category_title:
       description: Category title
       type: str
