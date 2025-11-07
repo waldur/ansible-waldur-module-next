233a6363b696193ff0bb5c6ffce9ad7586611491
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-11-07 21:08:29 +0000

    Update Ansible Waldur modules from develop branch

--- ansible_waldur_module/ansible_collections/waldur/openstack/plugins/modules/volume.py
+++ ansible_waldur_module/ansible_collections/waldur/openstack/plugins/modules/volume.py
@@ -478,8 +478,8 @@ RUNNER_CONTEXT = {
     "update_fields": ["bootable", "description", "name"],
     "attribute_param_names": [
         "availability_zone",
-        "image",
         "type",
+        "image",
         "description",
         "name",
         "size",
